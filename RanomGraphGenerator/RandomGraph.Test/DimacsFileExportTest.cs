94067350801d8b21bcd7465733c84fa30e14702b
drazenp <drazenp86@gmail.com>
2016-03-27 18:35:06 +0200

    Fixed export format file for the dimacs file type.

--- RanomGraphGenerator/RandomGraph.Test/DimacsFileExportTest.cs
+++ RanomGraphGenerator/RandomGraph.Test/DimacsFileExportTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -11,16 +12,20 @@ namespace RandomGraph.Test
         {
             var dummyGraph = new Dictionary<Vertex, List<Edge>>
             {
-                {new Vertex(0, 1), new List<Edge>() {new Edge(1, 1), new Edge(2, 4)}},
-                {new Vertex(1, 2), new List<Edge>() {new Edge(0, 3), new Edge(2, 1)}},
-                {new Vertex(2, 1), new List<Edge>() {new Edge(0, 1), new Edge(1, 3)}}
+                {new Vertex(0, 1), new List<Edge> {new Edge(1, 1), new Edge(2, 4)}},
+                {new Vertex(1, 2), new List<Edge> {new Edge(0, 3), new Edge(2, 1)}},
+                {new Vertex(2, 1), new List<Edge> {new Edge(0, 1), new Edge(1, 3)}}
             };
 
             return dummyGraph;
         }
 
+        private static readonly string ExportDimacsFile = "c Random generated graph."
+                                                        + $"{Environment.NewLine}p edge 3 6"
+                                                        + $"{Environment.NewLine}a 1 1 1 2 4"                                                        + $"{Environment.NewLine}a 2 0 3 2 1"                                                        + $"{Environment.NewLine}a 1 0 1 1 3";
+
         [TestMethod]
-        public void ExportGraph_WriteDataCalled()
+        public void ExportGraph_WriteData_Called()
         {
             var writerMock = new Mock<IDataWriter>();
                 writerMock.Setup(metod => metod.WriteData(It.IsAny<string>()));
@@ -30,6 +35,19 @@ namespace RandomGraph.Test
             dimacsFileExport.ExportGraph(dummyGraph);
 
             writerMock.Verify(method => method.WriteData(It.IsAny<string>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void ExportGraph_WriteData_CorrectFormat()
+        {
+            var writerMock = new Mock<IDataWriter>();
+            writerMock.Setup(metod => metod.WriteData(It.IsAny<string>()));
+
+            var dimacsFileExport = new DimacsFileExport(writerMock.Object);
+            var dummyGraph = GetDummyGraph();
+            dimacsFileExport.ExportGraph(dummyGraph);
+
+            writerMock.Verify(method => method.WriteData(ExportDimacsFile), Times.Once);
         }
     }
 }
--- RanomGraphGenerator/RandomGraph/DimacsFileExport.cs
+++ RanomGraphGenerator/RandomGraph/DimacsFileExport.cs
@@ -18,12 +18,12 @@ namespace RandomGraph
         public void ExportGraph(Dictionary<Vertex, List<Edge>> graph)
         {
             var sb = new StringBuilder(GetComment());
-            sb.AppendLine(GetProblemLine(graph));
+            sb.Append(GetProblemLine(graph));
 
             // The arc descriptors.
             foreach (var vertexEdgePair in graph)
             {
-                sb.Append($"{Environment.NewLine} {vertexEdgePair.Key.Weight}");
+                sb.Append($"{Environment.NewLine}a {vertexEdgePair.Key.Weight}");
                 foreach (var edge in vertexEdgePair.Value)
                 {
                     sb.Append($" {edge.VertexID} {edge.Weight}");
@@ -45,7 +45,7 @@ namespace RandomGraph
             // The number of arcs in the network.
             var arcs = graph.SelectMany(x => x.Value).Count();
 
-            return $"p edge {nodes} {arcs}";
+            return $"{Environment.NewLine}p edge {nodes} {arcs}";
         }
     }
 }
