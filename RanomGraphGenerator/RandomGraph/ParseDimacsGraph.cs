7c94e3ceb603c381cdab82b77cd2e71113fc81a1
drazenp <drazenp86@gmail.com>
2016-04-14 07:22:19 +0200

    Implemented dimacs parsing graph. Not tested.

--- RanomGraphGenerator/RandomGraph/ParseDimacsGraph.cs
+++ RanomGraphGenerator/RandomGraph/ParseDimacsGraph.cs
@@ -1,21 +1,52 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RandomGraph
 {
     public class ParseDimacsGraph : ParseGraphBase
     {
-        public ParseDimacsGraph(IDataLoader dataLoader) : base(dataLoader)
+        const int VertexWeight = 1;
+        const int EdgeWeight = 1;
+
+        public ParseDimacsGraph(IDataLoader dataLoader) 
+            : base(dataLoader)
         {
         }
-    }
 
-    public abstract class ParseGraphBase
-    {
-        public IDataLoader DataLoader { get; set; }
-
-        protected ParseGraphBase(IDataLoader dataLoader)
+        public override Dictionary<Vertex, List<Edge>> ParseGraph()
         {
-            DataLoader = dataLoader;
+            var graph = new Dictionary<Vertex, List<Edge>>();
+
+            foreach (var line in DataLoader.LoadData())
+            {
+                var fileData = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (fileData[0] == "e")
+                {
+                    var vertexID = int.Parse(fileData[1]) - 1;
+                    var connectedVertexID = int.Parse(fileData[2]) - 1;
+
+                    Vertex? vertex = graph.Keys.FirstOrDefault(v => v.ID == vertexID);
+                    if (vertex == null)
+                    {
+                        vertex = new Vertex(vertexID, VertexWeight);
+                        graph.Add(vertex.Value, new List<Edge>());
+                    }
+                    else
+                    {
+                        Vertex? connectedVertex = graph.Keys.FirstOrDefault(v => v.ID == connectedVertexID);
+                        if (!connectedVertex.HasValue)
+                        {
+                            connectedVertex = new Vertex(connectedVertexID, VertexWeight);
+                            graph.Add(connectedVertex.Value, new List<Edge>());
+                        }
+                        graph.Single(p => p.Key.ID == connectedVertexID).Value.Add(new Edge(vertexID, EdgeWeight));
+                    }
+                }
+            }
+
+            return graph;
         }
     }
 }
---
+++ RanomGraphGenerator/RandomGraph/ParseGraphBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace RandomGraph
+{
+    public abstract class ParseGraphBase
+    {
+        public IDataLoader DataLoader { get; set; }
+
+        protected ParseGraphBase(IDataLoader dataLoader)
+        {
+            DataLoader = dataLoader;
+        }
+
+        public abstract Dictionary<Vertex, List<Edge>> ParseGraph();
+    }
+}
