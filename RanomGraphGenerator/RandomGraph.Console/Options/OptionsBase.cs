a4157986db4dffaf01f16d0c21d5d40dc34f2994
drazenp <drazenp86@gmail.com>
2016-04-15 05:59:35 +0200

    Added missing file.

---
+++ RanomGraphGenerator/RandomGraph.Console/Options/OptionsBase.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace RandomGraph.Console.Options
+{
+    abstract class OptionsBase
+    {
+        [Option('f', HelpText = "Type of the graph output file.", Required = true)]
+        public GraphFileType ExportGraphFileType { get; set; }
+
+        [Option('o', HelpText = "The file name of the graph output file.", Required = true)]
+        public string ExportGraphFileName { get; set; }
+    }
+}
